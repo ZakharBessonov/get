c38db726ede6032bd80193dca68d6e6b7da4de1f
ZakharBessonov <zakhar200702@gmail.com>
2026-03-03 11:32:36 +0300

    Задание 4.3 выполнено

---
+++ Лабораторная работа №4 "ЦАП"/pwm_dac.py
@@ -0,0 +1,42 @@
+import RPi.GPIO as GPIO
+
+class PWM_DAC:
+    def __init__(self, gpio_pin, pwm_frequency, dynamic_range, verbose = False):
+        self.gpio_pin = gpio_pin
+        self.pwm_frequency = pwm_frequency
+        self.dynamic_range = dynamic_range
+        self.verbose = verbose
+
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(self.gpio_pin, GPIO.OUT, initial = 0)
+        self.pwm = GPIO.PWM(self.gpio_pin, self.pwm_frequency)
+        self.pwm.start(0)
+
+    def deinit(self):
+        GPIO.output(self.gpio_pin, 0)
+        GPIO.cleanup()
+
+    def set_number(self, number):
+        coef = number / self.dynamic_range * 100.0
+        print("Коэффициент заполнения: ", coef)
+        self.pwm.ChangeDutyCycle(coef)
+
+    def set_voltage(self, voltage):
+        if not (0.0 <= voltage <= self.dynamic_range):
+            print(f"Напряжение выходит за динамический диапазон ЦАП (0.00 - {self.dynamic_range:.2f} B)")
+            return None
+        self.set_number(voltage)
+
+if __name__ == "__main__":
+    try:
+        dac = PWM_DAC(12, 500, 3.29, True)
+
+        while True:
+            try:
+                voltage = float(input("Введите напряжение в вольтах: "))
+                dac.set_voltage(voltage)
+
+            except ValueError:
+                print("Вы ввели не число. Попробуйте снова\n")
+    finally:
+        dac.deinit()
